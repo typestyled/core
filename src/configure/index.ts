7c9246f1ccb2577b6be50da111d84c61a0b1d055
Frederik Krautwald <fkrautwald@gmail.com>
2017-10-24 14:40:57 +0200

    Expose API
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>

---
+++ src/configure/index.ts
@@ -0,0 +1,17 @@
+/*!
+ * Copyright © 2017 The Typestyled Authors. Licensed under the MIT license.
+ * See LICENSE.txt in the project root for complete license information.
+ *
+ * File:    index.ts
+ *
+ * Authors: Frederik Krautwald <fkrautwald@gmail.com>
+ * Date:    October 24, 2017
+ *
+ * Description:
+ *
+ * This file re-exports configure functions.
+ */
+
+export * from './computeLineHeight'
+export * from './computeSpacingUnit'
+export * from './configure'
